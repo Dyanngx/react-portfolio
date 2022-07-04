dd9a94ffa783e7aabd6d8931e36fae63c74a7bd0
dinhtuanng <dinhtuannguyen469@gmail.com>
2022-07-04 15:36:38 +0700

    update code navigation bar

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,42 +1,73 @@
-import React from 'react';
-import './contact.css';
+import React from "react";
+import "./contact.css";
 
 const Contact = () => {
   return (
-    <div className='container contact__container'>
-      <h2 className='title'>
-        Message Me!
-      </h2>
-      <p>
-        If you have any thing to talk with me, please contact me!
-      </p>
-      <form action="" id="Contact__Form">
-        <input type="hidden"/>
-        <input type="hidden"/>
-        <div className='contact__fields'>
-          <div className='field'>
-            <input type="text" className='field__input' autoComplete='name' id='ContactForm-name' placeholder='Name'/>
-            <label className='field__label' for="ContactForm-name">Name</label>
+    <section id="contact">
+      <div className="container contact__container">
+        <h2 className="title">Message Me!</h2>
+        <p>If you have any thing to talk with me, please contact me!</p>
+        <form action="" id="Contact__Form">
+          <input type="hidden" />
+          <input type="hidden" />
+          <div className="contact__fields">
+            <div className="field">
+              <input
+                type="text"
+                className="field__input"
+                autoComplete="name"
+                id="ContactForm-name"
+                placeholder="Name"
+              />
+              <label className="field__label" for="ContactForm-name">
+                Name
+              </label>
+            </div>
+            <div className="field field--with--error">
+              <input
+                type="text"
+                className="field__input"
+                autoComplete="email"
+                id="ContactForm-email"
+                placeholder="Email"
+              />
+              <label className="field__label" for="ContactForm-email">
+                Email<span>*</span>
+              </label>
+            </div>
           </div>
-          <div className='field field--with--error'>
-          <input type="text" className='field__input' autoComplete='email' id='ContactForm-email' placeholder='Email'/>
-            <label className='field__label' for="ContactForm-email">Email<span>*</span></label>
+          <div className="field">
+            <input
+              className="field__input"
+              type="tel"
+              name="contact"
+              id="ContactForm-phone"
+              autoComplete="tel"
+            />
+            <label className="field__label" for="ContactForm-phone" />
           </div>
-        </div>
-        <div className='field'>
-          <input className='field__input' type="tel" name="contact" id="ContactForm-phone" autoComplete='tel' />
-          <label className='field__label' for="ContactForm-phone"></label>
-        </div>
-        <div className='field'>
-          <textarea className='text-area field__input' name="contact" id="ContactForm-body" cols="30" rows="10"></textarea>
-          <label className='form__label field__label' for="ContactForm-body"></label>
-        </div>
-        <div className='contact__button'>
-          <button type='submit' className='button'>Submit</button>
-        </div>
-      </form>
-    </div>
-  )
-}
+          <div className="field">
+            <textarea
+              className="text-area field__input"
+              name="contact"
+              id="ContactForm-body"
+              cols="30"
+              rows="10"
+            />
+            <label
+              className="form__label field__label"
+              for="ContactForm-body"
+            />
+          </div>
+          <div className="contact__button">
+            <button type="submit" className="button">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
 
 export default Contact
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,23 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {IoMdContact} from 'react-icons/io'
+import  {MdOutlineHomeRepairService} from 'react-icons/md'
+import {GoBook} from 'react-icons/go'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import { useState } from 'react'
 
 const Nav = () => {
+  const  [activeNav,setActiveNav] = useState('#');
   return (
-    <div>Nav</div>
+    <nav>
+        <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+        <a href="#contact" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><IoMdContact /></a>
+        <a href="#exp" onClick={() => setActiveNav('#exp')} className={activeNav === '#exp' ? 'active' : ''}><GoBook /></a>
+        <a href="#services"  onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdOutlineHomeRepairService /></a>
+        <a href="#testimonials"  onClick={() => setActiveNav('#testimonials')} className={activeNav === '#testimonials' ? 'active' : ''}><GiHamburgerMenu /></a> 
+    </nav>
   )
 }
 
