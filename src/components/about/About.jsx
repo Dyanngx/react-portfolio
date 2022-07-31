b8f5290d3c7184d13355de60cbd443f97bdbe788
Dyanngx <Dyanngx@gmail.com>
2022-07-31 09:55:32 +0700

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,11 +3,7 @@ import './about.css';
 
 const About = () => {
   return (
-<<<<<<< HEAD
-    <section id='about'></section>
-=======
     <section id='about'>About</section>
->>>>>>> dd9a94ffa783e7aabd6d8931e36fae63c74a7bd0
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -3,11 +3,7 @@ import './testimonials.css'
 
 const Testimonials = () => {
   return (
-<<<<<<< HEAD
-    <section id='testimonials'></section>
-=======
     <section id='testimonials'>Testimonials</section>
->>>>>>> dd9a94ffa783e7aabd6d8931e36fae63c74a7bd0
   )
 }
 
